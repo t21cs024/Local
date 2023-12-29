44f356bbae9276d475ffa7ee041e6522b1c3b264
翼 <@192.168.2.199>
2023-12-29 15:38:49 +0900

    [add] mail作成,qr疑似作成
    在庫数がある値未満であり，メールを送信していない商品について発注メールを送信します．
    希望納品日はメール送信日の１週間後にしてあります．
    動作テストのためにカート画面を作成してありますが，画面遷移のみで在庫のデクリメントなどの機能はありません．
    
    ----以下は動作確認に必要な情報----
    
    ＜動作確認＞
    
    ①user.txtをbd.sqliteと同じフォルダに作成(ハードコードを避けるため，settings.pyでuser.txtを読み込んでいます）
    
    ---user.txtの中身---
    your_gmail@gmail（送信元メールアドレスになります．必ずgmail）
    your_passward（16文字のアプリパスワード)
    ---user.txtは以上--
    ※アプリパスワード作成の参考文献（https://codelab.website/django-send-gmail/ ）
    
    ②adminから企業(Company)を２社登録(Company id は1と2)
    ！！ 以下のCompany mailは自分で管理できるメールアドレスを入力してください ！！
    
    Company id＝1のCompany mail にはuser.txtに記述したgmailアドレスを登録してください（送信元メールアドレス）
    Company id＝2のCompany mail には送信先メールアドレスを記述してください（gmail以外も可）
    （id = 1 が自社，id = 2が発注先の企業という想定です）
    ③Itemの登録
    
    CountがMinimum amount未満である　かつ　Send email = False のItemを追加（複数可）
    ④userhome->「食品購入」->「カートに移動する」->「注文確定」
    
    「注文確定」を押した段階で，条件を満たす商品についてメールを送信します．受信フォルダを確認してみてください

--- superuserhome/views.py
+++ superuserhome/views.py
@@ -1,24 +1,28 @@
-#from django.views.generic import ListView
-from .models import User,Item,PurchaseHistory
+from .models import User,Item,PurchaseHistory,Company
 from django.views.generic.base import TemplateView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
-#from django.views.generic.edit import CreateView,UpdateView,DeleteView
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls.base import reverse_lazy
-#from django.utils.decorators import method_decorator
-#from django.contrib.auth.decorators import login_required, user_passes_test
-#from .forms import SignUpForm,UserIdForm,ItemBuy,ItemIdForm, ItemForm
 from .forms import SignUpForm,UserIdForm,ItemBuy,MonthForm
-#CSV関連のライブラリ
+#CSV関連
 import csv
 # test
 from django.http import HttpResponse
+# 発注メール送信関連
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.db.models import F
+from datetime import datetime, timedelta
+# test
 from pip._vendor.typing_extensions import Self
+import qrcode
+import os
 
-# Create your views here.
+# Create your views here.   
 class SuperUserHomeView(TemplateView):
     model = User
     template_name = 'superuser_home.html'
@@ -118,13 +122,11 @@ class UserInformationView(TemplateView):
 class UserInformationDetailView(TemplateView):
     model = User
     template_name = "Edit/userinformation_detail.html"
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form_id'] = UserIdForm()
         context['form_month'] = MonthForm()
         return context
-
     def get(self, request, *args, **kwargs):
         user_id = self.kwargs.get('user_id')
         user = get_object_or_404(User, pk=user_id)
@@ -134,8 +136,9 @@ class UserInformationDetailView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         user_id = self.request.POST.get('user_id')
-        user = get_object_or_404(User, user_id=user_id)
+        get_object_or_404(User, user_id=user_id)
         return HttpResponseRedirect(reverse('superuserhome:userinformation_detail', kwargs={'user_id': user_id}))
+
 
 from django.contrib import messages
 
@@ -202,12 +205,116 @@ class OldItemView(TemplateView):
     template_name = "Edit/Item/olditem.html"
     fields =('name')
 
-
-
     def get(self, request, *args, **kwargs):
         object_list = Item.objects.all()
         return render(request, self.template_name, {'object_list':object_list})
     
+class OrderConfirmedView(TemplateView):
+    template_name = "Order/buy_item.html"
+
+    def get(self, request):
+        # 在庫が重み付けによって決められた個数未満であり、発注メールを送信していないItemオブジェクトを取得
+        items_below_amount = Item.objects.filter(count__lt=F('minimum_amount'), send_email = False)
+        # 条件に該当するオブジェクトが存在する場合、発注メール送信メソッド呼び出し
+        if items_below_amount.exists():
+            self.send_order_mail(request, items_below_amount)
+        return redirect('userhome:buyitem')
+
+    def send_order_mail(self, request, items_below_amount):
+        # 自社（発注元企業）オブジェクトの取得 見つからない場合は404(発注元企業のIDは1を想定)
+        own_company = get_object_or_404(Company, company_id = 1)
+        # 発注先企業オブジェクトの取得 見つからない場合は404(発注企業のIDは2を想定)
+        supplier_company = get_object_or_404(Company, company_id = 2)
+
+        """題名"""
+        subject = f"商品注文のお願い（{own_company.company_name})"
+        """本文"""
+        message = (
+            f"{supplier_company.company_name}\n"
+            f"ご担当　{supplier_company.manager_name}様\n"
+            "\n"
+            "いつもお世話になっております。\n"
+            f"{own_company.company_name}の{own_company.manager_name}でございます。\n"
+            "\n"
+            "このたびは貴社の商品を発注したく連絡いたしました。\n"
+            "なお、具体的には以下の内容にて発注を考えており、ご了承いただいたのち注文書を発行いたします。ご確認いただければ幸いです。\n"
+            )
+        roop_count = 0
+        for item in items_below_amount:
+            # 発注する商品が複数ある場合
+            if items_below_amount.count() >= 2:
+                message += (
+                    "\n"
+                    f"商品{roop_count+1}：\n"
+                    )
+            else:
+                message += ("\n")
+            message += (
+                f"・商品名　：{item.name}\n"
+                f"・商品ID　：{item.id}\n"
+                f"・数　量　：{item.order_quantity}\n"
+                )
+            # 発注メールを送信したitemはフラグを立てておく（重複メール送信を防ぐため）
+            # 商品が到着し、在庫情報追加する際Falseに戻してください
+            item.send_email = True
+            item.save()
+            roop_count+=1
+
+        # 現在の日付から納品希望日の計算（発注メールを送信した１週間後を指定）
+        delivery_date = datetime.now() + timedelta(weeks=1)
+        # フォーマットを整える
+        desired_delivery_date = delivery_date.strftime("%Y年%m月%d日")
+        if items_below_amount.count() >= 2:
+            message += "\n"
+        message += (
+            f"・納入場所：弊社  {own_company.company_name}（住所：{own_company.company_address}）\n"
+            f"・納品希望日：{desired_delivery_date}\n"
+            "\n"
+            "ご不明な点がございましたら、"
+            f"担当の{own_company.manager_name}（連絡先：{own_company.manager_phone_number} / {own_company.manager_mail}）までご連絡くださいませ。\n"
+            "何卒、よろしくお願い申し上げます。\n"
+            "\n"
+            "────────────────────────\n"
+            f"{own_company.company_name}\n"
+            f"{own_company.manager_name}\n"
+            f"{own_company.company_address}\n"
+            f"TEL：{own_company.manager_phone_number}\n"
+            f"Email：{own_company.manager_mail}\n"
+            "URL：https://www.ycc.co.jp/\n"
+            "────────────────────────\n"
+            )
+
+        """送信元メールアドレス（企業DBから取得）"""
+        from_email = own_company.company_mail
+
+        """宛先メールアドレス（企業DBから取得）"""
+        recipient_list = [
+            supplier_company.company_mail
+            ]
+        send_mail(subject, message, from_email, recipient_list)    
+    
+class QrCodeView(TemplateView):
+    model = Item
+    template_name = "Edit/Item/qrcode/qrcode.html"
+    
+    def get(self, request, *args, **kwargs):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(str('item_id'))
+        qr.make(fit=True)
+
+        # 生成したQRコードをHttpResponseに設定
+        img = qr.make_image(fill_color="black", back_color="white")
+        response = HttpResponse(content_type="image/png")
+        img.save(response, format="PNG")
+            
+        return response
+    
+        
     
     
     
