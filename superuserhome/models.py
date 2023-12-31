daae6fca2648e911ee4fae893d316156edd43d1a
Yuha Sakamoto <t21cs024@yamanashi.ac.jp>
2023-12-31 20:52:40 +0900

    [clean]不要なコメントを削除
    models.pyに不要なコメントが残っていたので削除しました

--- superuserhome/models.py
+++ superuserhome/models.py
@@ -28,24 +28,12 @@ class User(models.Model):
 class Item(models.Model):
     '''
     データ例
-<<<<<<< HEAD
+    
     Name     ：apple
     Item url ：apple.png
     Count    ：10
     Price    ：100
     State    ：在庫あり    
-=======
-    
-    Name     ：apple
-    Item url ：apple.png
-    Count    ：10
-    Buy date ：2023-12-20
-    Price    ：100
-    Buy      ：False    
-    Order quantity ：5
-    Minimum amount ：5
-    Send email     ：False
->>>>>>> refs/remotes/choose_remote_name/ログイン機能追加
     '''
     name = models.CharField(max_length=100)
     item_url = models.URLField(blank = True,null = True)
